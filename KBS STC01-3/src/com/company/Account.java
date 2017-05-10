97a11abc380c35a989a966cefc267c5da351c9fc
pjotter32 <pjotter32@hotmail.com>
2017-05-10 10:09:28 +0200

    1e Start java

---
+++ KBS STC01-3/src/com/company/Account.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Date;
+
+/**
+ * Created by User on 9-5-2017.
+ */
+public class Account {
+    private int accountId;
+    private String naam;
+    private Date geboortedatum;
+    private String woonplaats;
+    private String wachtwoord;
+    private String telefoonnummer;
+    private String emailAdres;
+}
---
+++ KBS STC01-3/src/com/company/Koerier.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by User on 9-5-2017.
+ */
+public class Koerier extends Account{
+    private int puntensaldo;
+    private int totaalGeleverd;
+    private int rating;
+
+}
