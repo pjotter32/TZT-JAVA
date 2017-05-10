2477da3de63f2ae78dd2195b1daa82db1a8cd70e
pjotter32 <pjotter32@hotmail.com>
2017-05-10 11:55:28 +0200

    hoi
    hoi

--- KBS STC01-3/src/com/company/Account.java
+++ KBS STC01-3/src/com/company/Account.java
@@ -13,4 +13,72 @@ public class Account {
     private String wachtwoord;
     private String telefoonnummer;
     private String emailAdres;
+
+    public Account(int accountId, String naam, Date geboortedatum, String woonplaats, String wachtwoord, String telefoonnummer, String emailAdres) {
+        this.accountId = accountId;
+        this.naam = naam;
+        this.geboortedatum = geboortedatum;
+        this.woonplaats = woonplaats;
+        this.wachtwoord = wachtwoord;
+        this.telefoonnummer = telefoonnummer;
+        this.emailAdres = emailAdres;
+    }
+
+    //Getters + Setters
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public Date getGeboortedatum() {
+        return geboortedatum;
+    }
+
+    public void setGeboortedatum(Date geboortedatum) {
+        this.geboortedatum = geboortedatum;
+    }
+
+    public String getWoonplaats() {
+        return woonplaats;
+    }
+
+    public void setWoonplaats(String woonplaats) {
+        this.woonplaats = woonplaats;
+    }
+
+    public String getWachtwoord() {
+        return wachtwoord;
+    }
+
+    public void setWachtwoord(String wachtwoord) {
+        this.wachtwoord = wachtwoord;
+    }
+
+    public String getTelefoonnummer() {
+        return telefoonnummer;
+    }
+
+    public void setTelefoonnummer(String telefoonnummer) {
+        this.telefoonnummer = telefoonnummer;
+    }
+
+    public String getEmailAdres() {
+        return emailAdres;
+    }
+
+    public void setEmailAdres(String emailAdres) {
+        this.emailAdres = emailAdres;
+    }
 }
--- KBS STC01-3/src/com/company/Bestelling.java
+++ KBS STC01-3/src/com/company/Bestelling.java
@@ -10,4 +10,62 @@ public class Bestelling {
     private String eindLocatie;
     private int puntenWaarde;
     private int waardeBestelling;
+
+    public Bestelling(int klantId, int maatDoos, String beginLocatie, String eindLocatie, int puntenWaarde, int waardeBestelling) {
+        this.klantId = klantId;
+        this.maatDoos = maatDoos;
+        this.beginLocatie = beginLocatie;
+        this.eindLocatie = eindLocatie;
+        this.puntenWaarde = puntenWaarde;
+        this.waardeBestelling = waardeBestelling;
+    }
+
+    //Getters + Setters
+    public int getKlantId() {
+        return klantId;
+    }
+
+    public void setKlantId(int klantId) {
+        this.klantId = klantId;
+    }
+
+    public int getMaatDoos() {
+        return maatDoos;
+    }
+
+    public void setMaatDoos(int maatDoos) {
+        this.maatDoos = maatDoos;
+    }
+
+    public String getBeginLocatie() {
+        return beginLocatie;
+    }
+
+    public void setBeginLocatie(String beginLocatie) {
+        this.beginLocatie = beginLocatie;
+    }
+
+    public String getEindLocatie() {
+        return eindLocatie;
+    }
+
+    public void setEindLocatie(String eindLocatie) {
+        this.eindLocatie = eindLocatie;
+    }
+
+    public int getPuntenWaarde() {
+        return puntenWaarde;
+    }
+
+    public void setPuntenWaarde(int puntenWaarde) {
+        this.puntenWaarde = puntenWaarde;
+    }
+
+    public int getWaardeBestelling() {
+        return waardeBestelling;
+    }
+
+    public void setWaardeBestelling(int waardeBestelling) {
+        this.waardeBestelling = waardeBestelling;
+    }
 }
